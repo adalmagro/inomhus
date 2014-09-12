3c6328e79e9c18361a5fcbccec10e2966ef90bdf
adiaz <arturo.diaz@centum.es>
2014-09-12 21:55:02 +0200

    Package init

--- Inomhus/app/src/main/java/com/adalmagro/inomhus/datamodel/Bike.java
+++ Inomhus/app/src/main/java/com/adalmagro/inomhus/datamodel/Bike.java
@@ -1,7 +1,88 @@
 package com.adalmagro.inomhus.datamodel;
 
+import java.util.Vector;
+
 /**
  * Created by arturo.diaz on 08/09/2014.
  */
 public class Bike {
+
+    private class Crank {
+        protected Vector<Integer> teeth; //0-small; 1-mid; 2-big
+
+        Crank(int numPlates) {
+            this.teeths = new Vector<Integer>(numPlates);
+        }
+
+        /**
+         *
+         * @return Number of plates. -1 if not configured
+         */
+        int getNumPlates() {
+            if (teeth != null) {
+                return teeth.size();
+            }
+            else {
+                return -1;
+            }
+        }
+
+        int setTeethOf(int plate, int teeth) {
+            if (this.teeth == null) {
+                this.teeth = new Vector<Integer>(plate+1);
+            }
+            if (plate > 0 && plate < getNumPlates()-1) {
+                return teeth.set(plate, new Integer(teeth));
+            } else {
+                return -1;
+            }
+        }
+
+        int getTeethOf(int plate) {
+            if (plate > 0 && plate < getNumPlates()-1) {
+                return teeth.get(plate);
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    private class Cassette {
+        protected Vector<Integer> cassette;
+
+        Cassette(int num) {
+            cassette = new Vector<Integer>(num);
+        }
+
+        int setTeethOf(int ring, int teeth) {
+            if (this.cassette == null) {
+                this.cassette = new Vector<Integer>(ring+1);
+            }
+            if (ring > 0 && ring < getNumRings()-1) {
+                return cassette.set(ring, new Integer(teeth));
+            } else {
+                return -1;
+            }
+        }
+
+        int getTeethOf(int ring) {
+            if (ring > 0 && ring < getNumRings()-1) {
+                return cassette.get(ring);
+            } else {
+                return -1;
+            }
+        }
+
+        int getNumRings() {
+            if (cassette != null) {
+                return cassette.size();
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+    private int wheelSize; // in milimeters
+    private int crankPlates;
+
 }
--- Inomhus/app/src/main/java/com/adalmagro/inomhus/datamodel/Training.java
+++ Inomhus/app/src/main/java/com/adalmagro/inomhus/datamodel/Training.java
@@ -4,4 +4,36 @@ package com.adalmagro.inomhus.datamodel;
  * Created by arturo.diaz on 08/09/2014.
  */
 public class Training {
+
+    protected class Step {
+        protected int time; // in milis
+        protected Range hrRange;
+        protected Range cadRange;
+        protected Range spdRange;
+        protected Range pwrRange;
+    }
+
+    protected class Range {
+        private float minValue;
+        private float maxValue;
+
+        protected Range(float minValue, float maxValue) {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        boolean checkInRange(float value) {
+            if (value >= minValue && value <= maxValue) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private List<Step> steps;
+
+    public Training() {
+        this.steps = new List<Step>();
+    }
 }
